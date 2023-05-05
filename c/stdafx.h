2935c208a9a7e6bb1b0194050d2138653bf9d5d6
HYUNBIN KIM <kimhb0625@gmail.com>
2023-05-05 21:17:02 +0900

    Base_주석추가

--- c/stdafx.h
+++ c/stdafx.h
@@ -1,9 +1,11 @@
 #pragma once
 
+// scanf 같은 함수들은 안전하지 않기 때문에 경고를 무시하고 사용하기 위해
 #define _CRT_SECURE_NO_WARNINGS
 
+// c언어의 표준입출력 헤더
 #include <stdio.h>
 
-// Files
+// Files, Base에 선언한 함수에 접근하기 위해 포함
 #include "Base/Base.h"
 #include "DataType/DataType.h"
